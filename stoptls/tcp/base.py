6b87adf388d7e565cba7437a7011aca0c3f739c1
mathewmarcus <mathewmarcus456@gmail.com>
2018-11-10 17:37:39 -0600

    updated base TCP proxy

--- stoptls/tcp/base.py
+++ stoptls/tcp/base.py
@@ -1,22 +1,23 @@
 import socket
+import struct
 from abc import ABC, abstractmethod
 
 
 class TCPProxy(ABC):
     SO_ORIGINAL_DST = 80
 
-    def __init__(self, dst_port):
-        self.dst_port = dst_port
+    def __init__(self):
         super().__init__()
 
     @abstractmethod
     async def strip(self, client_reader, client_writer):
         raise NotImplementedError
 
-    def get_orig_ip(self, reader):
-        sock = reader._transport.get_extra_info('socket')
+    def get_orig_dst_socket(self, writer):
+        sock = writer.get_extra_info('socket')
         sockaddr_in = sock.getsockopt(socket.SOL_IP,
                                       TCPProxy.SO_ORIGINAL_DST,
                                       16)
+        port, = struct.unpack('!h', sockaddr_in[2:4])
         address = socket.inet_ntoa(sockaddr_in[4:8])
-        return address
+        return address, port
