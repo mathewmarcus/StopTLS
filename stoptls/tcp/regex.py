ad67e35ca7a376a13fb2f1656bb63d0bae51608d
mathewmarcus <mathewmarcus456@gmail.com>
2018-11-10 13:31:26 -0600

    added imap regexes

---
+++ stoptls/tcp/regex.py
@@ -0,0 +1,5 @@
+import re
+
+IMAP_COMMAND = re.compile('^(?P<tag>\S*) (?P<cmd>[A-Za-z]*)\r?\n$')
+IMAP_RESPONSE = re.compile('^(?P<tag>\S*) (?:(?P<ok>[Oo][Kk])|(?P<bad>[Bb][Aa][Dd])|(?P<no>[Nn][Oo]) )?(?P<response>.*)\r\n$')
+STARTTLS = re.compile('( ?)STARTTLS( ?)', flags=re.IGNORECASE)
