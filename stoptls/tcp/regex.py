23fa505691e93af7075071c5c1900b3b21362866
mathewmarcus <mathewmarcus456@gmail.com>
2018-11-10 17:37:50 -0600

    added SMTP regexes

--- stoptls/tcp/regex.py
+++ stoptls/tcp/regex.py
@@ -2,4 +2,7 @@ import re
 
 IMAP_COMMAND = re.compile('^(?P<tag>\S*) (?P<cmd>[A-Za-z]*)\r?\n$')
 IMAP_RESPONSE = re.compile('^(?P<tag>\S*) (?:(?P<ok>[Oo][Kk])|(?P<bad>[Bb][Aa][Dd])|(?P<no>[Nn][Oo]) )?(?P<response>.*)\r\n$')
-STARTTLS = re.compile('( ?)STARTTLS( ?)', flags=re.IGNORECASE)
+IMAP_STARTTLS = re.compile('( ?)STARTTLS( ?)', flags=re.IGNORECASE)
+
+SMTP_COMMAND = re.compile('^(?P<cmd>\S*)(?P<args> .*)\r?\n$')
+SMTP_RESPONSE = re.compile('^(?P<status_code>[0-9]{3})(?:(?P<line_cont>-)| )(?P<message>.*)?\r\n$')
