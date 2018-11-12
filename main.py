1742b7236b7436af957fc6cc4509eceef53ce575
mathewmarcus <mathewmarcus456@gmail.com>
2018-11-11 22:04:40 -0600

    added argparse

--- main.py
+++ main.py
@@ -6,10 +6,25 @@ from stoptls.tcp import TCPProxy
 
 
 parser = argparse.ArgumentParser(description='MitM proxy which performs opportunistic SSL/TLS stripping')
+parser.add_argument('--http-port',
+                    type=int,
+                    default=8080,
+                    help='HTTP listen port')
+parser.add_argument('-t', '--tcp-port',
+                    type=int,
+                    default=49151,
+                    help='TCP listen port')
+parser.add_argument('-p', '--tcp-protocols',
+                    default=['smtp', 'imap'],
+                    nargs='+',
+                    help='supported TCP protocols')
 
 
 if __name__ == '__main__':
+    args = parser.parse_args()
+
     loop = asyncio.get_event_loop()
     asyncio.ensure_future(web_main())
-    asyncio.ensure_future(TCPProxy.main(14314))
+    asyncio.ensure_future(TCPProxy.main(asyncio.start_server,
+                                        args.tcp_port))
     loop.run_forever()
